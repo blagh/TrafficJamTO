af345f7e4879c47ae21923f9f51115a684f30510
blagh <hannele.kormano@gmail.com>
2015-10-03 19:15:37 -0400

    add dataset, show map`

--- datarender.py
+++ datarender.py
@@ -1,13 +1,17 @@
-def get_data(folder='./datasets'):
-	pass
-	# open folder
-	# for each file in folder
-	# 	render image
-	#	render map
-	
+import os
+import matplotlib.pyplot as plt
+from pykml import parser
+
+def get_data(folder='./datasets', render=False):
+	maps = ['http://127.0.0.1:5000/datasets/DVP.kml']
+
+	graphs = []
+	for graph in graphs:
+		pass # render plot
+
+	return {'maps': maps, 'graphs': graphs}
+
+
+
 def render_plot(file):
 	pass
-	
-def render_map(file):
-	pass
-	
--- trafficjamto.py
+++ trafficjamto.py
@@ -1,39 +1,40 @@
-import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
-     abort, render_template, flash
-from datarender.py import get_data
+     abort, render_template, flash, send_from_directory
+from datarender import get_data
 
 DATABASE = '/tmp/trafficjamto.db'
 DEBUG = True
 GOOGLE_API_KEY = 'AIzaSyBtUhdT_gnCH1rsmc54NFm_nK4uscIqhOg'
 TOMTOM_API_KEY = 'toronto_trafficjam'
 
-# These are the "Tableau 20" colors as RGB.    
-TABLEAU20 = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),    
-             (44, 160, 44), (152, 223, 138), (214, 39, 40), (255, 152, 150),    
-             (148, 103, 189), (197, 176, 213), (140, 86, 75), (196, 156, 148),    
-             (227, 119, 194), (247, 182, 210), (127, 127, 127), (199, 199, 199),    
-             (188, 189, 34), (219, 219, 141), (23, 190, 207), (158, 218, 229)]    
-  
-# Scale the RGB values to the [0, 1] range, which is the format matplotlib accepts.    
-for i in range(len(TABLEAU20)):    
-    r, g, b = TABLEAU20[i]    
-    TABLEAU20[i] = (r / 255., g / 255., b / 255.)   
+# These are the "Tableau 20" colors as RGB.
+TABLEAU20 = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),
+             (44, 160, 44), (152, 223, 138), (214, 39, 40), (255, 152, 150),
+             (148, 103, 189), (197, 176, 213), (140, 86, 75), (196, 156, 148),
+             (227, 119, 194), (247, 182, 210), (127, 127, 127), (199, 199, 199),
+             (188, 189, 34), (219, 219, 141), (23, 190, 207), (158, 218, 229)]
+
+# Scale the RGB values to the [0, 1] range, which is the format matplotlib accepts.
+for i in range(len(TABLEAU20)):
+    r, g, b = TABLEAU20[i]
+    TABLEAU20[i] = (r / 255., g / 255., b / 255.)
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-def connect_db():
-    return sqlite3.connect(app.config['DATABASE'])
-
 @app.route("/")
 def index():
 	return render_template('index.html')
-	
+
+@app.route("/plot")
 @app.route("/plot/<render>")
 def plot(render=False):
 	data = get_data(file, render)
-	return render_template('plot.html', data)
+	return render_template('plot.html', maps=data['maps'], api_key=GOOGLE_API_KEY)
+
+@app.route("/datasets/<file>")
+def datasets(file):
+	return send_from_directory("./datasets/", file)
 
 if __name__ == "__main__":
 	app.run()
