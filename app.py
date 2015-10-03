9e043bebb2c58d62727de37a717084f05b47b93d
blagh <hannele.kormano@gmail.com>
2015-10-03 16:17:04 -0400

    Remove some moar stuff

--- app.py
+++ app.py
@@ -8,17 +8,6 @@ app.debug = True
 def index():
 	return 'Index Page'
 
-@app.route("/hello")
-@app.route("/hello/<name>")
-def hello(name = None):
-	return render_template('hello.html', name=name)
-	
-@app.route('/map')
-@app.route("/map/<lat>/<long>/<zoom>")
-def map(lat=None, long=None, zoom=None):
-	return render_template('map.html', lat=lat, long=long, zoom=zoom)
-
-
 @app.route('/plot')
 def plotIndex():
 	pass # show list of datasets available
