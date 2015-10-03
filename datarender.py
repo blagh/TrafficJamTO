4df45a0e2d68e1d1d79e9fd70ec923ee7242c54a
blagh <hannele.kormano@gmail.com>
2015-10-03 17:29:34 -0400

    more scaffolding, start of data render

---
+++ datarender.py
@@ -0,0 +1,13 @@
+def get_data(folder='./datasets'):
+	pass
+	# open folder
+	# for each file in folder
+	# 	render image
+	#	render map
+	
+def render_plot(file):
+	pass
+	
+def render_map(file):
+	pass
+	
--- trafficjamto.py
+++ trafficjamto.py
@@ -1,3 +1,10 @@
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template, flash
+from datarender.py import get_data
+
+DATABASE = '/tmp/trafficjamto.db'
+DEBUG = True
 GOOGLE_API_KEY = 'AIzaSyBtUhdT_gnCH1rsmc54NFm_nK4uscIqhOg'
 TOMTOM_API_KEY = 'toronto_trafficjam'
 
@@ -11,4 +18,22 @@ TABLEAU20 = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),
 # Scale the RGB values to the [0, 1] range, which is the format matplotlib accepts.    
 for i in range(len(TABLEAU20)):    
     r, g, b = TABLEAU20[i]    
-    TABLEAU20[i] = (r / 255., g / 255., b / 255.)    
+    TABLEAU20[i] = (r / 255., g / 255., b / 255.)   
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+@app.route("/")
+def index():
+	return render_template('index.html')
+	
+@app.route("/plot/<render>")
+def plot(render=False):
+	data = get_data(file, render)
+	return render_template('plot.html', data)
+
+if __name__ == "__main__":
+	app.run()
